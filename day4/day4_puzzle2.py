4f7ae2bd9f88e2f5e99fc033aa168049019cc144
erikdesilva <erik.j.desilva@gmail.com>
2020-12-06 10:34:10 -0700

    Minor refactor of VerifyPassports class
    Moved several variables from __init__ to class namespace and renamed using non-public class attribute conventions.

--- day4/day4_puzzle2.py
+++ day4/day4_puzzle2.py
@@ -8,40 +8,39 @@ input_txt = os.path.join(cwd, 'input.txt')
 
 class VerifyPassports():
 
+    _passport_params = {
+        'byr': [1920, 2002],
+        'iyr': [2010, 2020],
+        'eyr': [2020, 2030],
+        'hgt': {'cm': [150, 193],
+                'in': [59, 76]},
+        'hcl': '\#[0-9a-f]{6}',
+        'ecl': ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+        'pid': '^[0-9]{9}$'
+    }
+    _range_params = ['byr', 'iyr', 'eyr']
+    _regex_params = ['pid', 'hcl']
+
     def __init__(self, input_text):
 
         with open(input_txt, newline='\n') as input_file:
             lst_data = ''.join(input_file.read()).split('\n\n')
             self.lst_data = [i.replace('\n', ' ') for i in lst_data]
 
-        self.passport_params = {
-            'byr': [1920, 2002],
-            'iyr': [2010, 2020],
-            'eyr': [2020, 2030],
-            'hgt': {'cm': [150, 193],
-                    'in': [59, 76]},
-            'hcl': '\#[0-9a-f]{6}',
-            'ecl': ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
-            'pid': '^[0-9]{9}$'
-        }
-
-        self.range_params = ['byr', 'iyr', 'eyr']
-        self.regex_params = ['pid', 'hcl']
-
     def check_params(self, pass_dict):
         fields_valid = {}
-        for field in self.passport_params.keys():
+        for field in self._passport_params.keys():
             try:
                 field_val = pass_dict[field]
-                field_params = self.passport_params[field]
+                field_params = self._passport_params[field]
 
-                if field in self.range_params:
+                if field in self._range_params:
                     min_val = field_params[0]
                     max_val = field_params[1]
                     value = int(field_val)
                     fields_valid[field] = (
                         value >= min_val) and (value <= max_val)
-                elif field in self.regex_params:
+                elif field in self._regex_params:
                     fields_valid[field] = bool(match(field_params, field_val))
                 elif field == 'ecl':
                     fields_valid[field] = bool(field_val in field_params)
